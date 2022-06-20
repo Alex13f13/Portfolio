c91e4a8d4a89f89713ffe007a2f80123fb0f7384
Alejandro Feijóo <afeijoo@encamina.com>
2022-06-20 20:20:23 +0200

    About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './AboutStyle.css'
+import Buttons from '../Button/Button'
 
 export default function About() {
     return (
-        <div className='about-container'>
-            <h1>About</h1>
+        <div id="about" className='container about-container'>
+            <h2>
+                <span>About Me</span> <br />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Maiores consequuntur repudiandae eos amet quasi asperiores
+                    eligendi fugit minima iure quibusdam qui molestiae
+                    veritatis repellendus culpa eaque doloremque,
+                    doloribus sequi sunt.</p>
+            </h2>
+            <Buttons />
+            {/* <Technologies /> */}
         </div>
     )
 }
